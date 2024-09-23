88939218128bcb377b9bc5067f596c80cba90f04
Heeckeeta <heeckeeta@yandex.ru>
2024-09-23 14:48:34 +0400

    second commit

--- src/js/modal-call.js
+++ src/js/modal-call.js
@@ -5,12 +5,14 @@ let closeCall=document.querySelector('.close-call');
 let sidebar=document.querySelector('.sidebar');
 let main=document.querySelector('.main');
 let notSidebar=document.querySelector('.not-sidebar');
+let notModal=document.querySelector('.not-modal');
 
 
 sidebarCall.addEventListener('click', function(){
   call.classList.remove('hidden');
   sidebar.classList.add('blur');
   main.classList.add('blur');
+  notModal.classList.remove('hidden');
   if (window.innerWidth<1440){
     sidebar.classList.add('hidden');
     if (window.innerWidth>=768){
@@ -23,10 +25,28 @@ navigationCall.addEventListener('click', function(){
   call.classList.remove('hidden');
   sidebar.classList.add('blur');
   main.classList.add('blur');
+  notModal.classList.remove('hidden');
 });
 
 closeCall.addEventListener('click', function(){
   call.classList.add('hidden');
   sidebar.classList.remove('blur');
   main.classList.remove('blur');
+  notModal.classList.add('hidden');
+});
+
+ notModal.addEventListener('click', function(){
+   call.classList.add('hidden');
+   sidebar.classList.remove('blur');
+   main.classList.remove('blur');
+   notModal.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function(evt){
+  if ((!call.classList.contains('hidden')) && evt.keyCode===27){
+    call.classList.add('hidden');
+    sidebar.classList.remove('blur');
+    main.classList.remove('blur');
+    notModal.classList.add('hidden');
+  }
 });
